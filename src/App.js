d9794bc85e529932ba577dc39db1376b6a06d55a
saleembaig <gojalisaleem1@gmail.com>
2021-01-03 13:32:25 +0500

    created counter using react hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  //Using useState for increment value on button click
+  //useState(0) holds the initial value zero
+  //count has the current value
+  //setCount is a function and it has the updated value after clicking of the button
+  let [count, setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+  }
+  const DecNum = () => {
+    setCount(count - 1);
+  }
+  const Reset = () => {
+    setCount(count = 0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="div">
+        <h1 className='h1'>{count}</h1>
+        <button className="button1" onClick={IncNum}>Increment</button>
+        <br />
+        <button className="button2" onClick={DecNum}>Decrement</button>
+        <br />
+        <button className="button3" onClick={Reset}>Reset</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
